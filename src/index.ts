32e88656c1aaed6c01b63f63a4031baa41c2c420
tausifabid12 <tasifabid12@gmail.com>
2023-04-08 12:25:22 +0600

    deploy problem

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import{ resolvers} from './schema/resolvers.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const port = parseInt(process.env.PORT || "4000");
+
 
 
 console.log(process.env.ACCESS_TOKEN, 'this is token')
@@ -17,6 +19,6 @@ const server = new ApolloServer({
 
 console.log(process.env.ACCESS_TOKEN)
 
-const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+const { url } = await startStandaloneServer(server, { listen: { port: port } });
 
 console.log(`🚀 Server listening at: ${url}`);
